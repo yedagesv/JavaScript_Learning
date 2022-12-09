1210fe3885fd4bbdef614ae898299c2055d44c98
yedagesv <sitaramvyedage@gmail.com>
2022-12-09 21:18:15 +0530

    create Typeof data
    create new assignment on how to find which type of data is store in that.

---
+++ 01_typeOf_assign.js
@@ -0,0 +1,40 @@
+// Assignment on Type of Data Type 
+console.log("=========Assignment No-2===========");
+console.log("-------------Q-1.------------------");
+var bankName;
+console.log("//// before Initialize ////");
+console.log(bankName)
+
+bankName = "SBI Bank";
+console.log("//// after Initialize ////");
+console.log(bankName);
+
+var bankNameType = typeof bankName;
+console.log("//// Type of and console log ////");
+console.log(bankNameType);
+console.log(typeof bankName);
+
+console.log("---------------Q-2----------------");
+var marks = '90%';
+console.log("//// data Type on console ////");
+console.log(typeof marks);
+
+console.log("---------------Q-3----------------");
+var isWorking ;
+console.log("//// before Initialize ////");
+console.log(isWorking);
+var isWorking = true;
+console.log("//// after Initialize and log ////");
+console.log(isWorking);
+console.log("---------------Q-3(1)Data type of----- ");
+console.log(typeof isWorking);
+
+console.log("------Q-4(a)console.log and Data type------- ");
+var totalCount = "Hundred and Seven";
+console.log(totalCount);
+console.log(typeof totalCount);
+
+console.log("---------Q-4(a) update variable on console.log and Data type---------- ");
+var totalCount = 107;
+console.log(totalCount);
+console.log(typeof totalCount);
