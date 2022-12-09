e349cc9be801f8cdacc984b7948531024cb12632
yedagesv <sitaramvyedage@gmail.com>
2022-12-09 21:20:15 +0530

    Assignment on swapping variable

---
+++ 01_swap_variable_assign.js
@@ -0,0 +1,39 @@
+// Swap a variable or Exchange variable assignment 
+//before swap variables assign values
+console.log("Assignment-1");
+var name1 ="Sweety";
+name2 = "Cutie";
+console.log("===========Before Swap===========");
+console.log("name1 =",name1,"name2 =",name2);
+
+var temp ="Sweety";
+name1 = name2;
+name2 = temp;
+console.log("===========After Swap============");
+console.log("name1 =",name1,"name2 =",name2);
+
+
+
+
+
+
+
+
+
+
+console.log("Assignment-2");
+// Swap a variable or Exchange variable assignment 
+//before swap variables assign values
+var num1 = 1000;
+ num2 = 2000;
+ num3 = 3000;
+console.log("===========before Swap============");
+console.log("num1 =",num1,"num2 =",num2,"num3 =",num3);
+
+// after swap variables output values
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("===========After Swap=============");
+console.log("num1 =",num1,"num2 =",num2,"num3 =",num3);
