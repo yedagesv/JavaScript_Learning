7037126a991a9c4204027ee1c1a97fdb47656ad6
yedagesv <sitaramvyedage@gmail.com>
2022-12-09 21:21:24 +0530

    todays topic variable typeof how to identify

---
+++ 01_variable_typeof.js
@@ -0,0 +1,56 @@
+var myName; // Variable Declaration
+myName = "Mohit Sharma"; // Variable initialization
+console.log(myName);
+
+var myFrdName = "Sachin Tendulkar"; // Variable declaration and initialization at the same time
+console.log(myFrdName);
+
+myFrdName = "MS Dhoni"; // Variable update 
+console.log(myFrdName);
+
+var myFullName = "Mohit Sharma";
+
+console.log(myFullName);
+
+console.log("==== Learning typeof ====");
+var age = 31;
+var ageType = typeof age;
+console.log(ageType);
+console.log(typeof age);
+
+var pinCode = 431202;
+
+var city="Pune";
+
+var isMarried = false;
+
+console.log("==== Learning undefined====");
+var country;// variable declaration
+console.log(country);
+country = "India"; // Initialization
+console.log(country);
+
+var companyName;
+console.log(companyName);
+console.log(typeof companyName);
+
+console.log(typeof 45); var weight=45;
+console.log(typeof "Billgates"); 
+console.log(typeof false); var areYouMad = false;
+
+console.log(" Strongly typed vs loosely typed  ");
+// String microsoftOwner = "Billgates"; // Java
+// microsoftOwner = true;
+
+var microsoftOwner = "Billgates";
+console.log(typeof microsoftOwner); // 
+microsoftOwner = 45;
+console.log(typeof microsoftOwner);
+microsoftOwner = true;
+console.log(typeof microsoftOwner);
+
+
+
+
+
+
