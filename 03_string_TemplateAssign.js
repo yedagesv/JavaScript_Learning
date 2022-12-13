3499797f5ab5e9da3ff9c6c59a997170171020ef
yedagesv <sitaramvyedage@gmail.com>
2022-12-13 21:56:36 +0530

    Assignments on String Template
    here we use concatenation and how to find length of string ,no word, indexof,split,etc

---
+++ 03_string_TemplateAssign.js
@@ -0,0 +1,13 @@
+// function with no arguments and no returns type
+
+function StringTemplete() {
+    
+console.log(`MY dream company is "IBM"`);
+var myHobbies1 = "Playing Cricket";
+var myHobbies2 = "Reading Books";
+var myHobbies3 = "Learning New Things";
+console.log(`My Hobbies are: myhobbies1: ${myHobbies1} ,myhobbies2: ${myHobbies2} ,myHobbies3: ${myHobbies3}`);
+console.log(`Hobbies are concatinating: ${myHobbies1}${myHobbies2}${myHobbies3}`);
+}
+StringTemplete();
+
