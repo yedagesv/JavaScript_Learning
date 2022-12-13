05793e7b463c376011eacc2fe9c8eb7ff1770224
yedagesv <sitaramvyedage@gmail.com>
2022-12-13 22:01:25 +0530

    updated
    we can learn function expression
    with return or concatenation string Template or some other method to such as length, word ,chartAt ,etc.

--- 03_string.js
+++ 03_string.js
@@ -7,5 +7,88 @@ console.log("Char at index 3: ", firstName.charAt(3));
 
 var lengthTotal = firstName.length;
 
-console.log("Char at last index : ", firstName.charAt(lengthTotal-1));
+console.log("Char at last index : ", firstName.charAt(lengthTotal - 1));
 
+//function Experssion-storing a function as value
+
+var myName = function (num1, num2) {
+  //thise is ananomus function
+};
+myName(25, 30);
+
+var myName = function (num1, num2) {
+  //function Experssion  with return
+  var result = num1 * num2;
+  return result;
+};
+var myResult = myName(25, 30);
+
+console.log("======== concat() ==========");
+var firstName = "Mohit ";
+var lastName = "Sharma";
+// Concatenation using + operator
+var result = firstName + lastName;
+console.log("Concatenation using + operator:", result);
+var result = firstName.concat(" Ravesh ", lastName);
+console.log("Concatenation using + Oprator =", result);
+
+var greet = "Good Morning";
+var indexOf = greet.indexOf("M");
+console.log("Index of character M: ", indexOf);
+
+var indexOfNin = greet.indexOf("nin");
+console.log("Index of substring nin: ", indexOfNin);
+
+var greet = "Good Morning";
+var greetAfterReplace =  greet.replace("Morning", "Afternoon");
+console.log("Replace substring Morning with Afternoon:  ", greetAfterReplace);
+
+console.log(greet.replace("Replace substring",("Mor","Eve")));
+
+
+console.log("To upper case", greet.toUpperCase());
+console.log("To lower case", greet.toLowerCase());
+
+var greet = "    Good Morning     ";
+ console.log("Length ", greet.length);
+ var resultTrim = greet.trim();
+  console.log( "trim()", resultTrim, "Length: ", resultTrim.length);
+
+console.log("Include() ",greet.includes("Good"));
+
+var greet = "Good Morning";
+var resultSlice = greet.slice(0, 4);
+console.log("Slice method", resultSlice);
+
+console.log( "slice() with start index ",greet.slice(5));       // it will start 4 to (length-1)
+console.log( "slice with negative  start -index ",greet.slice(-5));
+console.log( "slice with negative  start -index ",greet.slice(-7,-2));          //end index is always higher than start one 
+
+
+var studList = "Dips|Jenny|Ganni|Manni|Shani";
+var studListSplit = studList.split("|");
+console.log(" Student list after split by char ,", studListSplit);
+console.log("Total number students: ", studListSplit.length);
+
+
+var studList = "Dips|Jenny|Ganni|Manni|Shani";
+var studListSplit = studList.split("|");
+console.log(" Student list after split by char ,", studListSplit);
+console.log("Total number students: ", studListSplit.length);
+
+var sentence = "Yes you can do it guys, just keep learning and practising as well";
+var wordsInSentence =  sentence.split(" ");
+console.log("Total words in sentence string is: ", wordsInSentence.length);
+
+
+console.log("======== String template ==========");
+console.log(`Yes you can do it "Sanket"  `);
+
+
+var firstName = "Mohit ";
+var lastName = "Sharma";
+console.log("First name is: "+ firstName + "Last name is: "+ lastName );
+
+console.log(`First name is: ${firstName} and last name is ${lastName}`);            // string templete with  variable subtitution 
+
+console.log("Hello","\nGood","\nMorning");
