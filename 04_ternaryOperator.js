b536bbcbc8c786f1a587117bdf45ed483159dbf3
yedagesv <sitaramvyedage@gmail.com>
2022-12-15 11:16:59 +0530

    Ternary Operator

---
+++ 04_ternaryOperator.js
@@ -0,0 +1,6 @@
+
+// If SSC marks greater than equal to 35 then pass or fail
+var sscMarks = 34;
+var resultPassOrFail = sscMarks>=35 ? "Congradulation you are Pass" : "You are Fail try next time";
+
+console.log(`Student Result Is : ${resultPassOrFail}`);
