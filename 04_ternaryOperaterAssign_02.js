44c20451ab3e50d2cd5d3f06c1e0caeab7c255f9
yedagesv <sitaramvyedage@gmail.com>
2022-12-16 10:50:20 +0530

    modified some output
    modified some output

--- 04_ternaryOperaterAssign_02.js
+++ 04_ternaryOperaterAssign_02.js
@@ -3,8 +3,10 @@ console.log(`--------------------Assignment No 2 ------------------------`);
 console.log(`-------Que No-1-----------Check Male Marriage Eligibility-----`);
 
 function maleMarriageEligibility(gender,age,boyname) {
-     var gende = (gender=="Male" && age>=21) ?"You are Eligible for Married" : "Sorry Guys Wait For some Time";
-     console.log(`Hey "${boyname}" ${gende}`);
+    console.log(`Personal Details : Name : ${boyname}, Gender : ${gender}, Age : ${age}`);
+
+     var gende = (gender=="Male" && age>=21) ? `Hey ${boyname} are Eligible for Marriage` : `Sorry ${boyname} Wait For some Time` ;
+     console.log(gende);                  // we can write in way also console.log(`Hey "${boyname}" ${gende}`);
      
 }
 maleMarriageEligibility("Male",25,"Billgates");
@@ -13,12 +15,14 @@ maleMarriageEligibility("Male",17,"Stew Jobs");
 console.log(`-------Que No-2-----------Check female Marriage Eligibility-----`);
 
 function femaleMarriageEligibility(gender,age,girlname) {
-    var gende = (gender=="Female" && age>=18) ?"You are Eligible for Married" : "Sorry Guys Wait For some Time";
+    console.log(`Personal Details : Name : ${girlname}, Gender : ${gender}, Age : ${age}`);
+    
+    var gende = (gender=="Female" && age>=18) ?`Hey ${girlname} You are Eligible for Married` : `Sorry ${girlname} Wait For some Time`;
     return gende;
 }
 var final = femaleMarriageEligibility("Female",16,"Jenifer");
-console.log(`Hey "Jenifer" ${final}`);
+console.log(final);
 var final = femaleMarriageEligibility("Female",27,"Malinda Gates");
-console.log(`Hey "Malinda Gates" ${final}`);
+console.log(final);
 
 
--- 04_ternaryOperatorAssign_01.js
+++ 04_ternaryOperatorAssign_01.js
@@ -2,38 +2,37 @@ console.log(`--------------------Assignment No 1 ------------------------`);
 console.log(`------------Que No-1----------------------Find Greater Number-----`);
 var checkNumber = function (num1,num2) {
     var greaterResult = (num1>num2) ? "YES" : "NO";
-    console.log(`The First value is greater than Second value : ${greaterResult} `);
+    console.log(`The ${num1} is greater than ${num2} : ${greaterResult} `);
 }
 checkNumber(10,-10);
 checkNumber(800,899);
 
 console.log(`------------Que No-2----------------------Check given No Even or ODD-----`);
 var checkEvenOdd = function (value) {
-    var result = (value%2==0) ? true : false;
+    var result = (value%2==0) ? `The value ${value} is even : True` : `The value ${value} is even : False`;
     return result;
 
 }
 var finalresult = checkEvenOdd(29);
-console.log(`The given value 29 is even : ${finalresult}`)
+console.log(finalresult)
 var finalresult = checkEvenOdd(44);
-console.log(`The given 44 value is even : ${finalresult}`)
+console.log(finalresult)
 var finalresult = checkEvenOdd(0);
-console.log(`The given value 0 is even : ${finalresult}`)
+console.log(finalresult)
 var finalresult = checkEvenOdd(101);
-console.log(`The given value 101 is even : ${finalresult}`)
+console.log(finalresult)
 
 
 console.log(`------------Que No-3----------------------Check string length EVEN or ODD-----`);
 
 var lengthEvenOdd = function (name) {
     var nameLength = name.length;
-    // console.log(`The length is : ${nameLength}`);
-    var check = (nameLength%2==0) ? "EVEN" : "ODD";
+    var check = (nameLength%2==0) ? `The string ${name} length is: ${nameLength} Which is : EVEN` : `The string ${name} length is: ${nameLength} Which is : ODD` ;
         return check;
 }
 var checkResult = lengthEvenOdd("JavaScript"); 
-console.log(`The string JavaScript length is : ${checkResult}`);
+console.log(checkResult);
 var checkResult = lengthEvenOdd("Developer");
-console.log(`The string Developer length is : ${checkResult}`);
+console.log(checkResult);
 var checkResult = lengthEvenOdd("Google");
-console.log(`The string Google length is : ${checkResult}`);
+console.log(checkResult);
