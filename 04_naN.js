75f989530c9ce05a696c083954a2c42247eeebc0
yedagesv <sitaramvyedage@gmail.com>
2022-12-16 10:49:50 +0530

    new session
    not a number , how to convert no to string and viceversa

---
+++ 04_naN.js
@@ -0,0 +1,13 @@
+
+var resultDivide = 0/0;
+console.log(`Result is: ${resultDivide}`);
+console.log(`Type of${resultDivide} is ${typeof resultDivide}`);
+
+
+var resultAdd = resultDivide + 10;
+console.log(`resultAdd is: ${resultAdd}`);
+
+var resultMul = resultDivide * 10;
+console.log(`resultMul is: ${resultMul}`);
+
+
