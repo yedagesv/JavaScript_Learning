cea0fb2227fb67f1875fe6b09dce0b128ee58231
yedagesv <sitaramvyedage@gmail.com>
2023-01-11 09:12:34 +0530

    New Session
    forEach ,how to use forEach method on Object and Promise

--- 12_forEach.js
+++ 12_forEach.js
@@ -35,3 +35,20 @@ array.forEach((currentValue) => {
 });
 console.log("Array of even numbers");
 console.log(arrayOfEven);
+
+
+
+console.log("Callback with arg functions");
+let add = function (n1, n2){
+    console.log(n1+n2);
+}
+let multiply = function (n1, n2){
+    console.log(n1*n2);
+}
+
+function operation(callbackAdd, callbackMultiply) {
+    console.log('operation');
+    callbackAdd(5, 5);
+    callbackMultiply(2, 2);
+} 
+operation(add, multiply);
